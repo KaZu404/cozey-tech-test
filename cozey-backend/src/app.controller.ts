9b3ec0cda28e8cbc42514a0d629c34853d86e872
KaZu404 <11072223+KaZu404@users.noreply.github.com>
2024-05-22 11:53:31 -0400

    Add getters for picking and packing teams + bruno api doc

--- cozey-backend/src/app.controller.ts
+++ cozey-backend/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './services/app.service';
 import { Order } from './models/order';
 import { LineItem } from './models/lineItem';
+import { Product } from './models/product';
 
 @Controller()
 export class AppController {
@@ -14,13 +15,32 @@ export class AppController {
 
   @Get('/packing-team/orders')
   getPackingTeamOrders(): Order[] {
-    // TODO: map to a different data model
     return this.appService.getOrders();
   }
 
   @Get('/product-picking-team/orders-products')
-  getOrders(): Order[] {
-    // TODO: map to a different data model
-    return this.appService.getOrders();
+  getOrders(): { product: Product; count: number }[] {
+    const orders = this.appService.getOrders();
+
+    const allProductsInOrders = orders
+      .map(({ lineItems }) => lineItems)
+      .reduce((previous, current) => [...previous, ...current])
+      .map(({ products }) => products)
+      .reduce((previous, current) => [...previous, ...current]);
+
+    const resultProductList: { product: Product; count: number }[] = [];
+
+    allProductsInOrders.forEach((productInOrder) => {
+      const productInResultList = resultProductList.find(
+        (productInList) => productInList.product.uuid === productInOrder.uuid,
+      );
+      if (productInResultList) {
+        productInResultList.count++;
+      } else {
+        resultProductList.push({ product: productInOrder, count: 1 });
+      }
+    });
+
+    return resultProductList;
   }
 }
